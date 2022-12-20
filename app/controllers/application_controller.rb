3eb1ab00be58d378fa9dc750a90f4ccecce7e40f
Nick Phelps <nickphelps34@gmail.com>
2022-12-20 15:18:25 -0500

    commit tree a80da9feb7c5921b0cb01ce5391597eade468e29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def show
+    session[:page_view] ||= 0
+    session[:page_view] += 1
+  
+  end
+
 end
